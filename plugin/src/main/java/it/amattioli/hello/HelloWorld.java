1dd6ca895a693595ce69f4888ab1b540e55d7e2b
a.mattioli <a.mattioli@reply.it>
2016-05-29 15:13:43 +0200

    plugin folder

---
+++ plugin/src/main/java/it/amattioli/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package it.amattioli.hello;
+
+import static org.apache.commons.lang3.StringUtils.*;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println(center("Hello World!",20,"#"));
+	}
+	
+}
